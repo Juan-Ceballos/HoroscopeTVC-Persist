e0d5cfcc5af93f3f8018d1d6ab2f830506cee1cb
Juan C <juanceballos@pursuit.org>
2020-01-15 13:37:51 -0500

    fc

---
+++ Horoscope2/HoroscopeVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Horoscope2
+//
+//  Created by Juan Ceballos on 1/15/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class HoroscopeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Horoscope2Tests/Horoscope2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Horoscope2Tests.swift
+//  Horoscope2Tests
+//
+//  Created by Juan Ceballos on 1/15/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import XCTest
+@testable import Horoscope2
+
+class Horoscope2Tests: XCTestCase {
+
+    
+    
+}
