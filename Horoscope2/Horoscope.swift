7ce2ccfa43ee80c7125e18fa94b8c8b7c79198b5
Juan C <juanceballos@pursuit.org>
2020-01-16 07:16:48 -0500

    added horoscope object

---
+++ Horoscope2/Horoscope.swift
@@ -0,0 +1,14 @@
+//
+//  Horoscope.swift
+//  Horoscope2
+//
+//  Created by Juan Ceballos on 1/16/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import Foundation
+
+struct Horoscope: Decodable {
+    let sunsign: String
+    let horoscope: String
+}
