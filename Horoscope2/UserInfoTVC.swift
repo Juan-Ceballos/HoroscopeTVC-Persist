de6db8a636f662770de52041af76ddc6134a3bb5
Juan C <juanceballos@pursuit.org>
2020-01-16 07:09:09 -0500

    segue button to vc that should display horoscope

--- Horoscope2/UserInfoTVC.swift
+++ Horoscope2/UserInfoTVC.swift
@@ -12,24 +12,17 @@ class UserInfoTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
-
+    
+    
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let screenHeight = UIScreen.main.bounds.size.height
+        return screenHeight / 16
     }
 
     /*
