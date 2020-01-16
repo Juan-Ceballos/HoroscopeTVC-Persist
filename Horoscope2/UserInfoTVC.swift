bb41b954781e5a34ed187eb159922fd949b8619c
Juan C <juanceballos@pursuit.org>
2020-01-16 11:36:47 -0500

    added array for cell labels

--- Horoscope2/UserInfoTVC.swift
+++ Horoscope2/UserInfoTVC.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class UserInfoTVC: UITableViewController {
 
+    // array for sunsigns, dynamic tableview
+    // view should appear
+    
+    var horoscopesLabels = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagattarius", "Capricorn", "Aquarius", "Pisces"]
+    
     var horoscopes = [Horoscope]()
     
     override func viewDidLoad() {
@@ -25,32 +30,42 @@ class UserInfoTVC: UITableViewController {
     
     func loadHoroscope()    {
         // aries should be horoscope pressed in tableview
-        HoroscopeAPI.fetchHoroscope(horoscope: "aries") { (result) in
+        guard let indexPath = tableView.indexPathForSelectedRow
+            else    {
+                fatalError()
+        }
+        
+        let selectedSign = tableView.cellForRow(at: indexPath)?.textLabel?.text?.lowercased() ?? ""
+            
+        HoroscopeAPI.fetchHoroscope(horoscope: selectedSign) { (result) in
             switch result   {
             case .failure(let appError):
                 print(appError)
             case .success(let horoscope):
                 self.horoscopes = horoscope
+                print(horoscope)
             }
         }
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "signCell", for: indexPath)
 
-        // Configure the cell...
+        let horoscope = horoscopesLabels[indexPath.row]
 
+        cell.textLabel?.text = horoscope
+        
         return cell
     }
 
 
     // MARK: - Navigation
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
+    //}
+    
 
 }
